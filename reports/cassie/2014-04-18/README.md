bdf794a48cea2b284a06fd56de92b0c9b7466a85
joyrexus <joyrexus@gmail.com>
2014-04-18 14:02:30 -0500

    add cassie request

---
+++ reports/cassie/2014-04-18/README.md
@@ -0,0 +1,11 @@
+# 2014-04-18 
+
+Cassie needs to align her coding on the original transcripts with the canonical
+dataset utterances and their unique IDs.  We're providing the utterance and IDs
+for all of the transripts she's coded.
+
+The [`visits.txt`](visits.txt) file contains a list of all visits that Cassie
+has coded.
+
+The [`query.py`](query.py) file contains the script used to dump the needed
+utterances/IDs from the LDP dataset.
---
+++ reports/cassie/2014-04-18/query.py
@@ -0,0 +1,20 @@
+from ldp.data import Utterances
+
+utterances = Utterances()
+visits = [x.rstrip().split('\t') for x in open('visits.txt')]
+
+columns = 'id, time, row, c_utts, p_utts'
+
+def stringify(args):
+    return '\t'.join(str(x) for x in args)
+
+for subj, sess in visits:
+    print 'output/{}.{:0>2}.xls ...'.format(subj, sess)
+    with open('output/{}.{:0>2}.xls'.format(subj, sess), 'w') as f:
+        header = stringify(('_' + columns.upper()).split(', ')) + '\n'
+        f.write(header)
+        cond = 'subject={} and session={}'.format(subj, sess)
+        for row in utterances(columns, cond, limit=''):
+            print stringify(row)
+            f.write(stringify(row) + '\n')
+
